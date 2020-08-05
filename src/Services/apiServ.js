ffea002f01632b77e4e162504070df89b9a25f9b
Eli <elionlink@gmail.com>
2020-08-05 19:26:03 +0300

    050820

--- src/Services/apiServ.js
+++ src/Services/apiServ.js
@@ -1,5 +1,5 @@
 export const getProducts = async (query) => {
-  const url = `http://localhost:3001/api/products/${query}`;
+  const url = `https://msbit-backend-eli.herokuapp.com/api/products/${query}`;
   const resp = await fetch(url);
   const data = await resp.json();
   return data;
